78cd2c0881ebf54f6870123a19b8a3b191bb21ce
Ryan J. Salva <ryanjsalva@github.com>
2022-05-02 00:36:45 +0000

    Add comments and TBD notes

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main {
   public static void main(String[] args) {
     char[][] board = new char[3][3];
 
+    // create the board in memory as a 2D array
     for (int r = 0; r < 3; r++) {
       for (int c = 0; c < 3; c++) {
         board[r][c] = '-';
@@ -13,10 +14,11 @@ class Main {
     System.out.println("Let's play TTT!!");
 
     movePlayer(board);
-
   }
 
   public static void drawBoard(char[][] board) {
+
+    // draw the board as a 3x3 in the console
     for (int r = 0; r < 3; r++) {
       for (int c = 0; c < 3; c++) {
         System.out.print(board[r][c] + " ");
@@ -32,29 +34,34 @@ class Main {
     int col;
     boolean playerOne = true;
 
+    // loop through player turns until there's a winner
     while (true) {
       drawBoard(board);
 
+      // prompt the user for a row and column number
       System.out.print("Enter a row number (0, 1, or 2): ");
       row = inp.nextInt();
 
       System.out.print("Enter a column number (0, 1, or 2): ");
       col = inp.nextInt();
 
+      // don't allow players to overwrite taken spots
       if (board[row][col] != '-') {
         System.out.println("That spot is taken!");
         continue;
       }
 
-      if (playerOne) {
-        board[row][col] = 'x';
-        evaluateWin(board);
-        playerOne = false;
-      } else {
-        board[row][col] = 'o';
-        evaluateWin(board);
-        playerOne = true;
-      }
+      // TBD: add error checking for values other than 0, 1, or 2
+
+      // assign x to playerOne
+      board[row][col] = (playerOne) ? 'x' : 'o';
+
+      // evaluate for a winner
+      evaluateWin(board);
+
+      // switch players
+      playerOne = !playerOne;
+
 
     }
   }
@@ -67,9 +74,14 @@ class Main {
       for (int c = 1; c < 3; c++) {
         if (board[r][c] != board[r][c-1]) break;
         if (c >= 2) System.out.println("we have a winner!");
+        
+        // TBD: exit once we've found a winner
       }
-
     }
+
+    // TBD: evaluate columns
+    // TBD: evaluate diagonals
+    
   }
 
   
